fd4c873431cc643b81c5e07165bea05788d7968c
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-07-14 00:47:22 +0200

    Update models.py

--- reports/models.py
+++ reports/models.py
@@ -1,9 +1,4 @@
 from django.db import models
-
-# Create your models here.
-# reports/models.py
-
-from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
