fff8df769bb5321622cde38427e3af9b8d732485
martin-creator <martinlubowa@outlook.com>
2024-07-18 00:37:37 +0300

    update urls.py

--- project/urls.py
+++ project/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path 
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenVerifyView,TokenObtainPairView,TokenRefreshView
 
 from rest_framework import permissions
 # from drf_yasg.views import get_schema_view
@@ -48,6 +49,10 @@ urlpatterns = [
     
     path('api/auth/', include('dj_rest_auth.urls')),
     path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
     
     # path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
