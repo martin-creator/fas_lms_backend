e491efd4a5959d5b54264f77124746a958fdd445
martin-creator <martinlubowa@outlook.com>
2024-07-19 06:12:17 +0300

    update settings.py

--- project/settings.py
+++ project/settings.py
@@ -330,3 +330,14 @@ SIMPLE_JWT = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # Replace this git GMAIL SMTP settings in production
+
+
+# # Security
+
+# SECURE_SSL_REDIRECT = True
+# SECURE_HSTS_SECONDS = 3600
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# SECURE_HSTS_PRELOAD = True
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# CSRF_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
