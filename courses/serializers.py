fe2e84d36e09dfb4078a3788c7046e263c937b0a
martin-creator <martinlubowa@outlook.com>
2024-07-19 10:26:23 +0300

    Added lessson and course serializers

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,7 +1,7 @@
 # Create serializers for the Course model
 
 from rest_framework import serializers
-from .models import Course, CourseEnrollment, CourseCompletion
+from .models import Course, CourseEnrollment, CourseCompletion, Lesson, Quiz, Choice, LessonProgress, QuizProgress, Question
 
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,3 +17,34 @@ class CourseCompletionSerializer(serializers.ModelSerializer):
     class Meta:
         model = CourseCompletion
         fields = '__all__'
+
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+class QuizSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quiz
+        fields = '__all__'
+
+class ChoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Choice
+        fields = '__all__'
+
+class LessonProgressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LessonProgress
+        fields = '__all__'
+
+class QuizProgressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuizProgress
+        fields = '__all__'
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = '__all__'
+
