65ae0ded597a682ded7a4d6c300ead40801b1ef7
martin-creator <martinlubowa@outlook.com>
2024-07-19 11:46:19 +0300

    update course_services.py

--- courses/services/course_services.py
+++ courses/services/course_services.py
@@ -1,2 +1,10 @@
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.db.models import Count
+from courses.models import Course, CourseEnrollment, CourseCompletion, Lesson, LessonProgress, Quiz, QuizProgress, Question, Choice
+from courses.serializers import CourseSerializer, CourseEnrollmentSerializer, CourseCompletionSerializer, LessonSerializer, LessonProgressSerializer, QuizSerializer, QuizProgressSerializer, QuestionSerializer, ChoiceSerializer
+from courses.utils import DateTimeUtils
+from courses.helpers.course_helpers import CourseHelpers
+from courses.querying.course_query import CourseQuery
+from courses.settings.course_settings import CourseSettings
+from courses.reports.course_report import CourseReport
+
