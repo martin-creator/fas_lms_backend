47691c67e1385c26cdbe90e1bf3797114f76d9e6
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-07-20 13:52:13 +0200

    Update notification_controller.py

--- notifications/controllers/notification_controller.py
+++ notifications/controllers/notification_controller.py
@@ -117,3 +117,94 @@ class NotificationController:
         - dict: The user's notification settings.
         """
         return self.notification_service.get_notification_settings(user_id)
+        
+    def get_unread_notifications_count(self, user):
+        """
+        Retrieve the count of unread notifications for a user.
+
+        Args:
+        - user (User): The user to retrieve the unread notifications count for.
+
+        Returns:
+        - int: The count of unread notifications.
+        """
+        return self.notification_service.get_unread_notifications_count(user)
+
+    def create_notification_template(self, notification_type, template):
+        """
+        Create or update a notification template for a notification type.
+
+        Args:
+        - notification_type (NotificationType): The type of notification.
+        - template (str): The template content.
+
+        Returns:
+        - NotificationTemplate: The created or updated NotificationTemplate object.
+        """
+        return self.notification_service.create_notification_template(notification_type, template)
+
+    def get_notification_template(self, notification_type):
+        """
+        Retrieve the notification template for a notification type.
+
+        Args:
+        - notification_type (NotificationType): The type of notification.
+
+        Returns:
+        - str: The template content.
+        """
+        return self.notification_service.get_notification_template(notification_type)
+
+    def get_notification_types(self):
+        """
+        Retrieve all notification types available in the system.
+
+        Returns:
+        - QuerySet: A queryset of all notification types.
+        """
+        return self.notification_service.get_notification_types()
+
+    def subscribe_to_notifications(self, user, notification_types):
+        """
+        Subscribe a user to receive notifications of specific types.
+
+        Args:
+        - user (User): The user to subscribe.
+        - notification_types (list): List of NotificationType objects to subscribe to.
+        """
+        self.notification_service.subscribe_to_notifications(user, notification_types)
+
+    def unsubscribe_from_notifications(self, user, notification_types):
+        """
+        Unsubscribe a user from receiving notifications of specific types.
+
+        Args:
+        - user (User): The user to unsubscribe.
+        - notification_types (list): List of NotificationType objects to unsubscribe from.
+        """
+        self.notification_service.unsubscribe_from_notifications(user, notification_types)
+
+    def notify_followers(self, user_profile, notification_type, content_object=None, content='', url=''):
+        """
+        Notify followers of a user profile about an action.
+
+        Args:
+        - user_profile (UserProfile): The user profile whose followers will be notified.
+        - notification_type (NotificationType): The type of notification.
+        - content_object (Model, optional): The object related to the notification.
+        - content (str, optional): The content of the notification.
+        - url (str, optional): The URL related to the notification.
+        """
+        self.notification_service.notify_followers(user_profile, notification_type, content_object, content, url)
+
+    def notify_all_users(self, notification_type, content_object=None, content='', url=''):
+        """
+        Notify all users about an action.
+
+        Args:
+        - notification_type (NotificationType): The type of notification.
+        - content_object (Model, optional): The object related to the notification.
+        - content (str, optional): The content of the notification.
+        - url (str, optional): The URL related to the notification.
+        """
+        self.notification_service.notify_all_users(notification_type, content_object, content, url)
