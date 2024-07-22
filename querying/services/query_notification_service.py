a54135b4e535a2e6e9fd12623baf84d1d2b955fe
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-07-22 16:15:53 +0200

    Create query_notification_service.py

---
+++ querying/services/query_notification_service.py
@@ -0,0 +1,9 @@
+# services/query_notification_service.py
+
+class QueryNotificationService:
+    def notify_admins(self, message):
+        """
+        Notify administrators of critical errors.
+        """
+        # Implement your notification logic here, e.g., send an email or a message to a monitoring system
+        pass
