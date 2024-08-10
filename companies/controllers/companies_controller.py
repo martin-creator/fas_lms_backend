b163a6cfe4f6b1f0bdfffbbacca5c674e45e7ddb
martin-creator <martinlubowa@outlook.com>
2024-08-10 22:07:21 +0300

    update companies_controller.py and companies_services.py

--- companies/controllers/companies_controller.py
+++ companies/controllers/companies_controller.py
@@ -1,97 +1,122 @@
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.db.models import Count
-from events.models import Event, EventRegistration, EventFeedback
-from events.serializers import EventSerializer, EventRegistrationSerializer, EventFeedbackSerializer
-from events.settings.event_settings import EventSettings
-from events.querying.event_query import EventQuery
-from events.utils import DateTimeUtils, UserUtils
-from events.reports.event_report import EventReport
-from events.services.event_services import EventService
+from companies.models import Company, CompanyUpdate
+from companies.serializers import CompanySerializer, CompanyUpdateSerializer    
+from companies.settings.companies_settings import CompanySettings
+from companies.querying.companies_query import CompanyQuery
+from companies.utils import UserUtils, DateTimeUtils
+from companies.reports.companies_report import CompanyReport
+from companies.services.companies_services import CompanyService
 
 
-
-class EventController:
+class CompanyController:
     
     def __init__(self):
-        self.event_query = EventQuery()
-        self.event_report = EventReport()
-        self.event_settings = EventSettings()
+        self.company_query = CompanyQuery()
+        self.company_report = CompanyReport()
+        self.company_settings = CompanySettings()
         self.user_utils = UserUtils()
         self.date_time_utils = DateTimeUtils()
-        self.event_service = EventService()
+        self.company_service = CompanyService()
+
+
+    def get_all_companies(self):
+        """
+        Get all companies.
+        """
+        return self.company_service.get_companies()
+
+
+    def get_company(self, company_id):
+        """
+        Get a specific company.
+        """
+        return self.company_service.get_company(company_id)
+
+
+
+# class EventController:
+    
+#     def __init__(self):
+#         self.event_query = EventQuery()
+#         self.event_report = EventReport()
+#         self.event_settings = EventSettings()
+#         self.user_utils = UserUtils()
+#         self.date_time_utils = DateTimeUtils()
+#         self.event_service = EventService()
 
     
-    def get_all_events(self):
-        """
-        Get all events.
-        """
-        return self.event_service.get_events
+#     def get_all_events(self):
+#         """
+#         Get all events.
+#         """
+#         return self.event_service.get_events
     
 
-    def get_event(self, event_id):
-        """
-        Get a specific event.
-        """
-        return self.event_service.get_event(event_id)
+#     def get_event(self, event_id):
+#         """
+#         Get a specific event.
+#         """
+#         return self.event_service.get_event(event_id)
     
-    def create_event(self, event_data):
-        """
-        Create a new event.
-        """
-        return self.event_service.create_event(event_data)
+#     def create_event(self, event_data):
+#         """
+#         Create a new event.
+#         """
+#         return self.event_service.create_event(event_data)
     
-    def update_event(self, event_id, event_data):
-        """
-        Update an event.
-        """
-        return self.event_service.update_event(event_id, event_data)
+#     def update_event(self, event_id, event_data):
+#         """
+#         Update an event.
+#         """
+#         return self.event_service.update_event(event_id, event_data)
     
-    def delete_event(self, event_id):
-        """
-        Delete an event.
-        """
-        return self.event_service.delete_event(event_id)
+#     def delete_event(self, event_id):
+#         """
+#         Delete an event.
+#         """
+#         return self.event_service.delete_event(event_id)
     
-    def delete_all_events(self):
-        """
-        Delete all events.
-        """
-        return self.event_service.delete_all_events()
+#     def delete_all_events(self):
+#         """
+#         Delete all events.
+#         """
+#         return self.event_service.delete_all_events()
     
-    def register_for_event(self, event_id, user_id):
-        """
-        Register for an event.
-        """
-        return self.event_service.register_for_event(event_id, user_id)
+#     def register_for_event(self, event_id, user_id):
+#         """
+#         Register for an event.
+#         """
+#         return self.event_service.register_for_event(event_id, user_id)
     
-    def unregister_from_event(self, event_id, user_id):
-        """
-        Unregister from an event.
-        """
-        return self.event_service.unregister_from_event(event_id, user_id)
+#     def unregister_from_event(self, event_id, user_id):
+#         """
+#         Unregister from an event.
+#         """
+#         return self.event_service.unregister_from_event(event_id, user_id)
     
-    def provide_event_feedback(self, event_id, user_id, feedback_data):
-        """
-        Provide feedback for an event.
-        """
-        return self.event_service.provide_event_feedback(event_id, user_id, feedback_data)
+#     def provide_event_feedback(self, event_id, user_id, feedback_data):
+#         """
+#         Provide feedback for an event.
+#         """
+#         return self.event_service.provide_event_feedback(event_id, user_id, feedback_data)
     
-    def get_event_attendees(self, event_id):
-        """
-        Get all attendees for an event.
-        """
-        return self.event_service.get_event_attendees(event_id)
+#     def get_event_attendees(self, event_id):
+#         """
+#         Get all attendees for an event.
+#         """
+#         return self.event_service.get_event_attendees(event_id)
     
-    def get_event_registrations(self, event_id):
-        """
-        Get all registrations for an event.
-        """
-        return self.event_service.get_event_registrations(event_id)
+#     def get_event_registrations(self, event_id):
+#         """
+#         Get all registrations for an event.
+#         """
+#         return self.event_service.get_event_registrations(event_id)
     
-    def get_event_feedbacks(self, event_id):
-        """
-        Get all feedbacks for an event.
-        """
-        return self.event_service.get_event_feedbacks(event_id)
+#     def get_event_feedbacks(self, event_id):
+#         """
+#         Get all feedbacks for an event.
+#         """
+#         return self.event_service.get_event_feedbacks(event_id)
     
 
--- companies/services/companies_services.py
+++ companies/services/companies_services.py
@@ -1,201 +1,221 @@
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.db.models import Count
-from events.models import Event, EventRegistration, EventFeedback
-from events.serializers import EventSerializer, EventRegistrationSerializer, EventFeedbackSerializer
-from events.settings.event_settings import EventSettings
-from events.querying.event_query import EventQuery
-from events.utils import DateTimeUtils, UserUtils
-from events.reports.event_report import EventReport
+from companies.models import Company, CompanyUpdate
+from companies.serializers import CompanySerializer, CompanyUpdateSerializer
+from companies.settings.companies_settings import CompanySettings
+from companies.querying.companies_query import CompanyQuery
+from companies.utils import UserUtils, DateTimeUtils
+from companies.reports.companies_report import CompanyReport
 
 
-class EventService:
+class CompanyService:
 
     @staticmethod
-    def get_events():
+    def get_companies():
         """
-        Get all events.
+        Get all companies.
         """
-        events = EventQuery.get_events()
-        return events
+        companies = CompanyQuery.get_companies()
+        return companies
     
 
     @staticmethod
-    def get_event(event_id):
+    def get_company(company_id):
         """
-        Get a specific event.
+        Get a specific company.
         """
-        event = EventQuery.get_event(event_id)
-        return event
+        company = CompanyQuery.get_company(company_id)
+        return company
 
-    @staticmethod
-    def create_event(event_data):
-        """
-        Create a new event.
-        """
 
-        event, event_tags = EventQuery.process_event_data(event_data)
-        event.save()
+# class EventService:
 
-        if event_tags:
-            event.tags.set(event_tags)
-
-        serializer = EventSerializer(event)
-
-        return serializer.data
+#     @staticmethod
+#     def get_events():
+#         """
+#         Get all events.
+#         """
+#         events = EventQuery.get_events()
+#         return events
     
 
-    @staticmethod
-    def update_event(event_id, event_data):
-        """
-        Update an event.
-        """
+#     @staticmethod
+#     def get_event(event_id):
+#         """
+#         Get a specific event.
+#         """
+#         event = EventQuery.get_event(event_id)
+#         return event
 
-        # event = EventQuery.get_event(event_id)
-        event, event_tags = EventQuery.process_event_update_data(event_id, event_data)
-        event.save()
+#     @staticmethod
+#     def create_event(event_data):
+#         """
+#         Create a new event.
+#         """
 
-        if event_tags:
-            event.tags.set(event_tags)
+#         event, event_tags = EventQuery.process_event_data(event_data)
+#         event.save()
 
-        serializer = EventSerializer(event)
+#         if event_tags:
+#             event.tags.set(event_tags)
 
-        return serializer.data
+#         serializer = EventSerializer(event)
+
+#         return serializer.data
     
 
-    @staticmethod
-    def delete_event(event_id):
-        """
-        Delete an event.
-        """
-        event = EventQuery.get_event(event_id)
-        event.delete()
+#     @staticmethod
+#     def update_event(event_id, event_data):
+#         """
+#         Update an event.
+#         """
 
-        return True
-    
-    
-    @staticmethod
-    def delete_all_events():
-        """
-        Delete all events.
-        """
-        events = EventQuery.get_events()
-        events.delete()
+#         # event = EventQuery.get_event(event_id)
+#         event, event_tags = EventQuery.process_event_update_data(event_id, event_data)
+#         event.save()
 
-        return True
+#         if event_tags:
+#             event.tags.set(event_tags)
+
+#         serializer = EventSerializer(event)
+
+#         return serializer.data
     
 
-    @staticmethod
-    def get_event_report(event_id):
-        """
-        Get a report for a specific event.
-        """
-        event = EventQuery.get_event(event_id)
-        report = EventReport.get_event_report(event)
+#     @staticmethod
+#     def delete_event(event_id):
+#         """
+#         Delete an event.
+#         """
+#         event = EventQuery.get_event(event_id)
+#         event.delete()
 
-        return report
+#         return True
+    
+    
+#     @staticmethod
+#     def delete_all_events():
+#         """
+#         Delete all events.
+#         """
+#         events = EventQuery.get_events()
+#         events.delete()
+
+#         return True
     
 
-    @staticmethod
-    def get_attendee_report(attendee_id):
-        """
-        Get a report for a specific attendee.
-        """
-        attendee = UserUtils.get_current_user(attendee_id)
-        report = EventReport.get_attendee_report(attendee)
+#     @staticmethod
+#     def get_event_report(event_id):
+#         """
+#         Get a report for a specific event.
+#         """
+#         event = EventQuery.get_event(event_id)
+#         report = EventReport.get_event_report(event)
 
-        return report
-    
-    @staticmethod
-    def register_for_event(event_id, attendee_id):
-        """
-        Register for an event.
-        """
-        event = EventQuery.get_event(event_id)
-        attendee = UserUtils.get_current_user(attendee_id)
-
-        registration = EventRegistration(event=event, attendee=attendee)
-        registration.save()
-
-        return True
-
-
-    @staticmethod
-    def unregister_from_event(event_id, attendee_id):
-        """
-        Unregister from an event.
-        """
-        registration = EventQuery.get_event_registration(event_id, attendee_id)
-        registration.delete()
-
-        return True
+#         return report
     
 
-    @staticmethod
-    def provide_event_feedback(event_id, attendee_id, feedback_data):
-        """
-        Provide feedback for an event.
-        """
-        event = EventQuery.get_event(event_id)
-        attendee = UserUtils.get_current_user(attendee_id)
+#     @staticmethod
+#     def get_attendee_report(attendee_id):
+#         """
+#         Get a report for a specific attendee.
+#         """
+#         attendee = UserUtils.get_current_user(attendee_id)
+#         report = EventReport.get_attendee_report(attendee)
 
-        feedback = EventFeedback(event=event, attendee=attendee, **feedback_data)
-        feedback.save()
-
-        return True
-
+#         return report
     
-    @staticmethod
-    def get_events_by_organizer(organizer_id):
-        """
-        Get all events organized by a specific organizer.
-        """
-        events = EventQuery.get_events_by_organizer(organizer_id)
-        return events
-    
+#     @staticmethod
+#     def register_for_event(event_id, attendee_id):
+#         """
+#         Register for an event.
+#         """
+#         event = EventQuery.get_event(event_id)
+#         attendee = UserUtils.get_current_user(attendee_id)
 
-    @staticmethod
-    def get_events_by_attendee(attendee_id):
-        """
-        Get all events attended by a specific attendee.
-        """
-        events = EventQuery.get_events_by_attendee(attendee_id)
-        return events
-    
+#         registration = EventRegistration(event=event, attendee=attendee)
+#         registration.save()
 
-    @staticmethod   
-    def get_event_attendees(event_id):
-        """
-        Get all attendees for an event.
-        """
-        attendees = EventQuery.get_event_attendees(event_id)
-        return attendees
+#         return True
 
 
-    @staticmethod
-    def get_event_registrations(event_id):
-        """
-        Get all registrations for a specific event.
-        """
-        registrations = EventQuery.get_event_registrations(event_id)
-        return registrations
+#     @staticmethod
+#     def unregister_from_event(event_id, attendee_id):
+#         """
+#         Unregister from an event.
+#         """
+#         registration = EventQuery.get_event_registration(event_id, attendee_id)
+#         registration.delete()
+
+#         return True
     
 
-    @staticmethod
-    def get_event_feedbacks(event_id):
-        """
-        Get all feedbacks for a specific event.
-        """
-        feedbacks = EventQuery.get_event_feedbacks(event_id)
-        return feedbacks
+#     @staticmethod
+#     def provide_event_feedback(event_id, attendee_id, feedback_data):
+#         """
+#         Provide feedback for an event.
+#         """
+#         event = EventQuery.get_event(event_id)
+#         attendee = UserUtils.get_current_user(attendee_id)
+
+#         feedback = EventFeedback(event=event, attendee=attendee, **feedback_data)
+#         feedback.save()
+
+#         return True
+
+    
+#     @staticmethod
+#     def get_events_by_organizer(organizer_id):
+#         """
+#         Get all events organized by a specific organizer.
+#         """
+#         events = EventQuery.get_events_by_organizer(organizer_id)
+#         return events
     
 
-    @staticmethod
-    def get_events_monthly_report():
-        """
-        Get a monthly report for all events.
-        """
-        report = EventReport.get_events_monthly_report()
-        return report
+#     @staticmethod
+#     def get_events_by_attendee(attendee_id):
+#         """
+#         Get all events attended by a specific attendee.
+#         """
+#         events = EventQuery.get_events_by_attendee(attendee_id)
+#         return events
+    
+
+#     @staticmethod   
+#     def get_event_attendees(event_id):
+#         """
+#         Get all attendees for an event.
+#         """
+#         attendees = EventQuery.get_event_attendees(event_id)
+#         return attendees
+
+
+#     @staticmethod
+#     def get_event_registrations(event_id):
+#         """
+#         Get all registrations for a specific event.
+#         """
+#         registrations = EventQuery.get_event_registrations(event_id)
+#         return registrations
+    
+
+#     @staticmethod
+#     def get_event_feedbacks(event_id):
+#         """
+#         Get all feedbacks for a specific event.
+#         """
+#         feedbacks = EventQuery.get_event_feedbacks(event_id)
+#         return feedbacks
+    
+
+#     @staticmethod
+#     def get_events_monthly_report():
+#         """
+#         Get a monthly report for all events.
+#         """
+#         report = EventReport.get_events_monthly_report()
+#         return report
     
 
     
