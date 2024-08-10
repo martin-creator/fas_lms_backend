232ccd4e62f711cbd835e8472752967b99b94182
martin-creator <martinlubowa@outlook.com>
2024-08-10 22:18:43 +0300

    update urls.py and views.py

--- companies/views.py
+++ companies/views.py
@@ -1,3 +1,129 @@
 from django.shortcuts import render
+from rest_framework import generics
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from companies.models import Company, CompanyUpdate
+from companies.serializers import CompanySerializer, CompanyUpdateSerializer
+from companies.controllers.companies_controller import CompanyController
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample, OpenApiResponse
+from drf_spectacular.types import OpenApiTypes
 
 # Create your views here.
+
+company_controller = CompanyController()
+
+
+@extend_schema(
+    parameters=[],
+    examples=[
+        OpenApiExample(
+            'Example 1',
+            summary='Get all companies',
+            description='Get all companies',
+            value={}
+        )
+    ],
+    request=CompanySerializer,
+    responses={200: OpenApiResponse(response=OpenApiTypes.OBJECT, description='List of companies')}
+)
+@api_view(['GET'])
+def get_companies(request):
+    """
+    API endpoint that allows all companies to be retrieved.
+    """
+    if request.method == 'GET':
+        companies = company_controller.get_all_companies()
+        return Response(companies, status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+# @extend_schema(
+#     parameters=[],
+#     examples=[
+#         OpenApiExample(
+#             'Example 1',
+#             summary='Get all courses',
+#             description='Get all courses',
+#             value={}
+#         )
+#     ],
+#     request=CourseSerializer,
+#     responses={200: OpenApiResponse(response=OpenApiTypes.OBJECT, description='List of courses')}
+# )
+# @api_view(['GET'])
+# def get_courses(request):
+#     """
+#     API endpoint that allows all courses to be retrieved.
+#     """
+#     if request.method == 'GET':
+#         courses = course_controller.get_all_courses()
+#         return Response(courses, status=status.HTTP_200_OK)
+#     else:
+#         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+
+# @extend_schema(
+#     parameters=[
+#         OpenApiParameter(name='course_id', type=int, location=OpenApiParameter.PATH, required=True),
+#     ],
+#     examples=[
+#         OpenApiExample(
+#             'Example 1',
+#             summary='Get a specific course',
+#             description='Get a specific course',
+#             value={}
+#         )
+#     ],
+#     request=CourseSerializer,
+#     responses={200: OpenApiResponse(response=OpenApiTypes.OBJECT, description='Course data')}
+# )
+# @api_view(['GET'])
+# def get_specific_course(request, course_id):
+#     """
+#     API endpoint that allows a specific course to be retrieved.
+#     """
+#     if request.method == 'GET':
+#         course = course_controller.get_course_by_id(course_id)
+#         return Response(course, status=status.HTTP_200_OK)
+#     else:
+#         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+
+
+# @csrf_exempt
+# @extend_schema(
+#     parameters=[
+#         OpenApiParameter(name='title', type=str, location=OpenApiParameter.QUERY, required=True),
+#         OpenApiParameter(name='description', type=str, location=OpenApiParameter.QUERY, required=True),
+#         OpenApiParameter(name='instructor', type=int, location=OpenApiParameter.QUERY, required=True),
+#         OpenApiParameter(name='categories', type=str, location=OpenApiParameter.QUERY, required=True),
+#         OpenApiParameter(name='tags', type=str, location=OpenApiParameter.QUERY, required=True),
+#         OpenApiParameter(name='duration', type=int, location=OpenApiParameter.QUERY, required=False),
+#         OpenApiParameter(name='level', type=str, location=OpenApiParameter.QUERY, required=False),
+#     ],
+#     examples=[
+#         OpenApiExample(
+#             'Example 1',
+#             summary='Create a new course',
+#             description='Create a new course',
+#             value={
+#                 "title": "title",
+#                 "description": "description",
+#                 "instructor_id": 1,
+#                 "categories": "categories",
+#                 "tags": "tags"
+#             }
+#         )
+#     ],
+#     request=CourseCreateSerializer,
+#     responses={201: OpenApiResponse(response=OpenApiTypes.OBJECT, description='Course data')}
+
+# )
+
+
+
