200c2b93be841fa006d02043393fea3f8207c9e2
martin-creator <martinlubowa@outlook.com>
2024-08-15 06:31:20 +0300

    update serializers.py

--- groups/serializers.py
+++ groups/serializers.py
@@ -1,14 +1,52 @@
 from rest_framework import serializers
-from .models import Company, CompanyUpdate
+from .models import Group, GroupMembership, Discussion, Message, Announcement, Meeting, Task, Project, Milestone
 
-class CompanySerializer(serializers.ModelSerializer):
+
+class GroupSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Company
+        model = Group
         fields = '__all__'
 
 
-class CompanyUpdateSerializer(serializers.ModelSerializer):
+class GroupMembershipSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CompanyUpdate
+        model = GroupMembership
         fields = '__all__'
+
+
+class DiscussionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Discussion
+        fields = '__all__'
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = '__all__'
+
+
+class AnnouncementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Announcement
+        fields = '__all__'
+
+
+class MeetingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Meeting
+        fields = '__all__'
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
 
