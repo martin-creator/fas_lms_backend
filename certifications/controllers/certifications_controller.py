5bfef86b7d545a1efa6d2d47410b577913840563
martin-creator <martinlubowa@outlook.com>
2024-08-22 07:49:25 +0300

    update certifications_controller.py

--- certifications/controllers/certifications_controller.py
+++ certifications/controllers/certifications_controller.py
@@ -1,185 +1,101 @@
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.db.models import Count
-from companies.models import Company, CompanyUpdate
-from companies.serializers import CompanySerializer, CompanyUpdateSerializer    
-from companies.settings.companies_settings import CompanySettings
-from companies.querying.companies_query import CompanyQuery
-from companies.utils import UserUtils, DateTimeUtils
-from companies.reports.companies_report import CompanyReport
-from companies.services.companies_services import CompanyService
+from certifications.models import Certification, LinkedInBadge
+from certifications.serializers import CertificationSerializer, LinkedInBadgeSerializer   
+from certifications.settings.certifications_settings import CertificationsSettings
+from certifications.querying.certifications_query import CertificationQuery
+from certifications.helpers.certifications_helpers import CertificationHelpers
+from certifications.utils import UserUtils, DateTimeUtils
+from certifications.reports.certifications_report import CertificationsReport
+from certifications.services.certifications_services import CertificationService
 
 
-class CompanyController:
-    
-    def __init__(self):
-        self.company_query = CompanyQuery()
-        self.company_report = CompanyReport()
-        self.company_settings = CompanySettings()
-        self.user_utils = UserUtils()
-        self.date_time_utils = DateTimeUtils()
-        self.company_service = CompanyService()
-
-
-    def get_all_companies(self):
-        """
-        Get all companies.
-        """
-        return self.company_service.get_companies()
-
-
-    def get_company_by_id(self, company_id):
-        """
-        Get a specific company.
-        """
-        return self.company_service.get_company(company_id)
-
-
-    def create_company(self, company_data):
-        """
-        Create a new company.
-        """
-        return self.company_service.create_company(company_data)
-    
-
-    def update_company(self, company_id, company_data):
-        """
-        Update a company.
-        """
-        return self.company_service.update_company(company_id, company_data)
-    
-
-    def delete_company(self, company_id):
-        """
-        Delete a company.
-        """
-        return self.company_service.delete_company(company_id)
-    
-
-    def delete_all_companies(self):
-        """
-        Delete all companies.
-        """
-        return self.company_service.delete_all_companies()
-    
-
-    def get_company_updates(self, company_id):
-        """
-        Get all updates for a company.
-        """
-        return self.company_service.get_company_updates(company_id)
-    
-
-    def get_company_update_by_id(self, update_id):
-        """
-        Get a specific company update.
-        """
-        return self.company_service.get_company_update_by_id(update_id)
-    
-
-    def create_company_update(self, company_id, update_data):
-        """
-        Create an update for a company.
-        """
-        return self.company_service.create_company_update(company_id, update_data)
-    
-
-    def update_company_update(self, company_id, update_id, update_data):
-        """
-        Update an update for a company.
-        """
-        return self.company_service.update_company_update(company_id, update_id, update_data)
-    
-
-    def delete_company_update(self, update_id):
-        """
-        Delete a company update.
-        """
-        return self.company_service.delete_company_update(update_id)
-
-
-
-# class EventController:
+# class CompanyController:
     
 #     def __init__(self):
-#         self.event_query = EventQuery()
-#         self.event_report = EventReport()
-#         self.event_settings = EventSettings()
+#         self.company_query = CompanyQuery()
+#         self.company_report = CompanyReport()
+#         self.company_settings = CompanySettings()
 #         self.user_utils = UserUtils()
 #         self.date_time_utils = DateTimeUtils()
-#         self.event_service = EventService()
+#         self.company_service = CompanyService()
 
-    
-#     def get_all_events(self):
+
+#     def get_all_companies(self):
 #         """
-#         Get all events.
+#         Get all companies.
 #         """
-#         return self.event_service.get_events
+#         return self.company_service.get_companies()
+
+
+#     def get_company_by_id(self, company_id):
+#         """
+#         Get a specific company.
+#         """
+#         return self.company_service.get_company(company_id)
+
+
+#     def create_company(self, company_data):
+#         """
+#         Create a new company.
+#         """
+#         return self.company_service.create_company(company_data)
     
 
-#     def get_event(self, event_id):
+#     def update_company(self, company_id, company_data):
 #         """
-#         Get a specific event.
+#         Update a company.
 #         """
-#         return self.event_service.get_event(event_id)
+#         return self.company_service.update_company(company_id, company_data)
     
-#     def create_event(self, event_data):
+
+#     def delete_company(self, company_id):
 #         """
-#         Create a new event.
+#         Delete a company.
 #         """
-#         return self.event_service.create_event(event_data)
+#         return self.company_service.delete_company(company_id)
     
-#     def update_event(self, event_id, event_data):
+
+#     def delete_all_companies(self):
 #         """
-#         Update an event.
+#         Delete all companies.
 #         """
-#         return self.event_service.update_event(event_id, event_data)
+#         return self.company_service.delete_all_companies()
     
-#     def delete_event(self, event_id):
+
+#     def get_company_updates(self, company_id):
 #         """
-#         Delete an event.
+#         Get all updates for a company.
 #         """
-#         return self.event_service.delete_event(event_id)
+#         return self.company_service.get_company_updates(company_id)
     
-#     def delete_all_events(self):
+
+#     def get_company_update_by_id(self, update_id):
 #         """
-#         Delete all events.
+#         Get a specific company update.
 #         """
-#         return self.event_service.delete_all_events()
+#         return self.company_service.get_company_update_by_id(update_id)
     
-#     def register_for_event(self, event_id, user_id):
+
+#     def create_company_update(self, company_id, update_data):
 #         """
-#         Register for an event.
+#         Create an update for a company.
 #         """
-#         return self.event_service.register_for_event(event_id, user_id)
+#         return self.company_service.create_company_update(company_id, update_data)
     
-#     def unregister_from_event(self, event_id, user_id):
+
+#     def update_company_update(self, company_id, update_id, update_data):
 #         """
-#         Unregister from an event.
+#         Update an update for a company.
 #         """
-#         return self.event_service.unregister_from_event(event_id, user_id)
+#         return self.company_service.update_company_update(company_id, update_id, update_data)
     
-#     def provide_event_feedback(self, event_id, user_id, feedback_data):
+
+#     def delete_company_update(self, update_id):
 #         """
-#         Provide feedback for an event.
+#         Delete a company update.
 #         """
-#         return self.event_service.provide_event_feedback(event_id, user_id, feedback_data)
-    
-#     def get_event_attendees(self, event_id):
-#         """
-#         Get all attendees for an event.
-#         """
-#         return self.event_service.get_event_attendees(event_id)
-    
-#     def get_event_registrations(self, event_id):
-#         """
-#         Get all registrations for an event.
-#         """
-#         return self.event_service.get_event_registrations(event_id)
-    
-#     def get_event_feedbacks(self, event_id):
-#         """
-#         Get all feedbacks for an event.
-#         """
-#         return self.event_service.get_event_feedbacks(event_id)
-    
+#         return self.company_service.delete_company_update(update_id)
+
+
 
