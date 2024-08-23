4237442ad59c28bafae7e516cc0eca7e854c269f
martin-creator <martinlubowa@outlook.com>
2024-08-23 03:35:32 +0300

    update urls.py

--- followers/urls.py
+++ followers/urls.py
@@ -1,19 +1,27 @@
 from django.urls import path
-from companies import views
+from followers import views
 
 urlpatterns = [
-    path('', views.get_companies, name='get_companies'),
-    path('companies/<int:company_id>/', views.get_specific_company, name='get_specific_company'),
-    path('companies/create/', views.create_company, name='create_company'),
-    path('companies/update/<int:company_id>/', views.update_company, name='update_company'),
-    path('companies/delete/<int:company_id>/', views.delete_company, name='delete_company'),
-    path('companies/delete/all/', views.delete_all_companies, name='delete_all_companies'),
-    path('companies/updates/<int:company_id>/', views.get_company_updates, name='get_company_updates'),
-    path('<int:update_id>/', views.get_specific_company_update, name='get_specific_company_update'),
-    path('companies/updates/create/<int:company_id>/', views.create_company_update, name='create_company_update'),
-    path('companies/updates/update/<int:company_id>/<int:update_id>/', views.update_company_update, name='update_company_update'),
-    path('companies/updates/delete/<int:update_id>/', views.delete_company_update, name='delete_company_update'),
-    # path('events/', views.get_events),
-    # path('events/<int:event_id>/', views.get_event),
-    # path('events/create/', views.create_event),
+    path('', views.get_followers, name='get_followers'),
+    path('<int:follower_id>/', views.get_specific_follower, name='get_specific_follower'),
+    path('user/<int:user_id>/', views.get_followers_by_user, name='get_followers_by_user'),
+    path('company/<int:company_id>/', views.get_followers_by_company, name='get_followers_by_company'),
+    path('user/<int:user_id>/company/<int:company_id>/', views.get_followers_by_user_and_company, name='get_followers_by_user_and_company'),
+    path('follow_requests/', views.get_follow_requests, name='get_follow_requests'),
+    path('follow_requests/<int:request_id>/', views.get_specific_follow_request, name='get_specific_follow_request'),
+    path('follow_requests/user/<int:user_id>/', views.get_follow_requests_by_user, name='get_follow_requests_by_user'),
+    path('follow_requests/company/<int:company_id>/', views.get_follow_requests_by_company, name='get_follow_requests_by_company'),
+    path('follow_requests/user/<int:user_id>/company/<int:company_id>/', views.get_follow_requests_by_user_and_company, name='get_follow_requests_by_user_and_company'),
+    path('follow_requests/', views.create_follow_request, name='create_follow_request'),
+    path('follow_requests/<int:request_id>/', views.update_follow_request, name='update_follow_request'),
+    path('follow_requests/<int:request_id>/', views.delete_follow_request, name='delete_follow_request'),
+    path('follow_notifications/', views.get_follow_notifications, name='get_follow_notifications'),
+    path('follow_notifications/<int:notification_id>/', views.get_specific_follow_notification, name='get_specific_follow_notification'),
+    path('follow_notifications/', views.create_follow_notification, name='create_follow_notification'),
+    path('follow_notifications/<int:notification_id>/', views.update_follow_notification, name='update_follow_notification'),
+    path('follow_notifications/<int:notification_id>/', views.delete_follow_notification, name='delete_follow_notification'),
+    path('follow_notifications/', views.delete_all_follow_notifications, name='delete_all_follow_notifications'),
+    path('follow_notifications/user/<int:user_id>/', views.get_follow_notifications_by_user, name='get_follow_notifications_by_user'),
+    path('follow_notifications/company/<int:company_id>/', views.get_follow_notifications_by_company, name='get_follow_notifications_by_company'),
+   
 ]
