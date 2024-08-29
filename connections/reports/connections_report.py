5a3313a9a469870fd0a43d4bcfcc6e1d23aa7808
martin-creator <martinlubowa@outlook.com>
2024-08-29 16:10:22 +0300

    delete 2 files and create 2 files

---
+++ connections/reports/connections_report.py
@@ -0,0 +1,145 @@
+from django.db.models import Count, Q, Sum, Avg, F, ExpressionWrapper, fields
+from connections.models import ConnectionRequest, Connection
+from connections.serializers import ConnectionRequestSerializer, ConnectionSerializer
+from connections.querying.connections_query import ConnectionQuery
+
+
+class ConnectionReport:
+    @staticmethod
+    def get_connection_requests_report():
+        """
+        Get a report for all connection requests.
+        """
+        connection_requests = ConnectionRequest.objects.all()
+        serializer = ConnectionRequestSerializer(connection_requests, many=True)
+        return serializer.data
+    
+    @staticmethod
+    def get_connections_report():
+        """
+        Get a report for all connections.
+        """
+        connections = Connection.objects.all()
+        serializer = ConnectionSerializer(connections, many=True)
+        return serializer.data
+    
+    @staticmethod
+    def get_connection_requests_by_user_report(user_id):
+        """
+        Get a report for all connection requests sent to a specific user.
+        """
+        connection_requests = ConnectionRequest.objects.filter(to_user=user_id)
+        serializer = ConnectionRequestSerializer(connection_requests, many=True)
+        return serializer.data
+    
+    @staticmethod
+    def get_connection_requests_by_status_report(status):
+        """
+        Get a report for all connection requests with a specific status.
+        """
+        connection_requests = ConnectionRequest.objects.filter(status=status)
+        serializer = ConnectionRequestSerializer(connection_requests, many=True)
+        return serializer.data
+    
+    @staticmethod
+    def get_connection_request_report(request_id):
+        """
+        Get a report for a specific connection request.
+        """
+        connection_request = ConnectionRequest.objects.get(id=request_id)
+        serializer = ConnectionRequestSerializer(connection_request)
+        return serializer.data
+    
+    @staticmethod
+    def get_connection_report(connection_id):
+        """
+        Get a report for a specific connection.
+        """
+        connection = Connection.objects.get(id=connection_id)
+        serializer = ConnectionSerializer(connection)
+        return serializer.data
+
+
+# class CompanyReport:
+#     @staticmethod
+#     def get_company_report(company):
+#         """
+#         Get a report for a specific company.
+#         """
+#         company_data = CompanySerializer(company).data
+#         company_updates = CompanyUpdate.objects.filter(company=company)
+#         company_data['updates'] = CompanyUpdateSerializer(company_updates, many=True).data
+
+#         # return json data
+
+#         json_data = {
+#             'company': company_data,
+#             'updates': company_data['updates']
+#         }
+
+#         return json_data
+
+#     @staticmethod
+#     def get_owner_report(owner):
+#         """
+#         Get a report for a specific owner.
+#         """
+#         owner_data = {}
+#         owner_data['companies'] = CompanyQuery.get_companies_by_owner(owner).count()
+#         owner_data['updates'] = CompanyQuery.get_company_updates_by_owner(owner).count()
+
+#         return owner_data
+
+#     @staticmethod
+#     def get_employees_report():
+#         """
+#         Get a report for all employees.
+#         """
+#         employees = Company.objects.all()
+#         serializer = CompanySerializer(employees, many=True)
+#         return serializer.data
+    
+#     @staticmethod
+#     def get_companies_location_report():
+#         """
+#         Get a location report for all companies.
+#         """
+#         companies = Company.objects.all()
+#         serializer = CompanySerializer(companies, many=True)
+#         return serializer.data
+    
+#     @staticmethod
+#     def get_companies_monthly_report():
+#         """
+#         Get a monthly report for all companies.
+#         """
+#         companies = Company.objects.all()
+#         serializer = CompanySerializer(companies, many=True)
+#         return serializer.data
+    
+#     @staticmethod
+#     def get_companies_industry_report():
+#         """
+#         Get an industry report for all companies.
+#         """
+#         companies = Company.objects.all()
+#         serializer = CompanySerializer(companies, many=True)
+#         return serializer.data
+    
+#     @staticmethod
+#     def get_companies_employees_report():
+#         """
+#         Get an employees report for all companies.
+#         """
+#         companies = Company.objects.all()
+#         serializer = CompanySerializer(companies, many=True)
+#         return serializer.data
+    
+#     @staticmethod
+#     def get_companies_owners_report():
+#         """
+#         Get an owners report for all companies.
+#         """
+#         companies = Company.objects.all()
+#         serializer = CompanySerializer(companies, many=True)
+#         return serializer.data
