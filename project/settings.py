7b39fcecf66cc31d711d879caeb3700e1326d7f5
Martin Joseph Lubowa <martinlubowa@outlook.com>
2024-09-28 05:47:18 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://localhost:5174',
     'https://fas-lms-backend.onrender.com',
+    'https://cors-test.codehappy.dev/',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
